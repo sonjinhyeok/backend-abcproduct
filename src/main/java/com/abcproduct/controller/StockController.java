b0bfcb1221a0cdcc6bb3826203e9edb6eec27a18
sonjinhyeok <jinhyeok.son@axas-japan.co.jp>
2021-04-14 18:49:24 +0900

    Stock List

--- src/main/java/com/abcproduct/controller/StockController.java
+++ src/main/java/com/abcproduct/controller/StockController.java
@@ -1,5 +1,7 @@
 package com.abcproduct.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,47 +14,12 @@ import com.abcproduct.domain.repository.StockRepository;
 public class StockController {
 
 	@Autowired
-	private StockRepository stockRepository;
+	private StockRepository mapper;
 
-	// 全体のデータを表示する
-	@GetMapping("/stock")
-	public String getStocks(Model model) {
-		Stock stock = stockRepository.selectByPrimaryKey(1);
+	@GetMapping("")
+	public String getProducts(Model model) {
+		List<Stock> stock = mapper.selectInfo();
 		model.addAttribute("stock", stock);
-		return "stock";
+		return "index";
 	}
-
-//	// 全体のデータを照会するAPI
-//		@GetMapping("/product/all")
-//		public List<Product> getProductList() {
-//		    return productRepository.getProductList();
-//	}
-//
-//	// idが一致するデータを照会API
-//	@GetMapping("/product/{product_id}")
-//	public Product getProduct(@PathVariable("product_id") int product_id) {
-//		return productRepository.getProduct(product_id);
-//	}
-//
-//	// 生成
-//	@PostMapping("/product/{product_id}")
-//	public void insertProduct(@PathVariable("product_id") int product_id,
-//							  @RequestParam("product_name") String product_name,
-//							  @RequestParam("product_price") int product_price) {
-//		productRepository.insertProduct(product_id, product_name, product_price);
-//	}
-//
-//	// 修正
-//	@PutMapping("/product/{product_id}")
-//	public void updateProduct(@PathVariable("product_id") int product_id,
-//							  @RequestParam("product_name") String product_name,
-//							  @RequestParam("product_price") int product_price) {
-//		productRepository.updateProduct(product_id, product_name, product_price);
-//	}
-//
-//	// 削除
-//	@DeleteMapping("/product/{product_id}")
-//	public void deleteProduct(@PathVariable("product_id") int product_id) {
-//		productRepository.deleteProduct(product_id);
-//	}
 }
