2683ac6b793587b8834ed24eb328f66117810459
sonjinhyeok <jinhyeok.son@axas-japan.co.jp>
2021-04-19 09:33:59 +0900

    Paging test

--- src/main/java/com/abcproduct/controller/InfoController.java
+++ src/main/java/com/abcproduct/controller/InfoController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.abcproduct.domain.model.Info;
 import com.abcproduct.domain.model.InfoExample;
 import com.abcproduct.domain.repository.InfoRepository;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 
 @Controller
 public class InfoController {
@@ -18,11 +21,15 @@ public class InfoController {
 	private InfoRepository mapper;
 	private InfoExample example;
 
-	@GetMapping("/")
-	public String getInfo(Model model) {
+	@GetMapping("")
+	public String getInfo(@RequestParam(required = false, defaultValue = "1") int pageNum, Model model) {
 		List<Info> info = mapper.selectByExample(example);
 		model.addAttribute("info", info);
+
+		PageHelper.startPage(pageNum,5);
+		Page<Info> pages = mapper.selectAll();
+		model.addAttribute("pages", pages);
+
 		return "index";
 	}
-
 }
