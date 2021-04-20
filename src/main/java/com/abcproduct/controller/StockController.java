6262b38a1f1f1537288a58ba44c75ab834c12a21
sonjinhyeok <jinhyeok.son@axas-japan.co.jp>
2021-04-20 11:23:16 +0900

    Data fixed

--- src/main/java/com/abcproduct/controller/StockController.java
+++ src/main/java/com/abcproduct/controller/StockController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.abcproduct.domain.model.Stock;
+import com.abcproduct.domain.paging.Criteria;
 import com.abcproduct.domain.repository.StockRepository;
 
 @Controller
@@ -15,10 +17,10 @@ public class StockController {
 
 	@Autowired
 	private StockRepository mapper;
-
 	@GetMapping("")
-	public String getProducts(Model model) {
-		List<Stock> stock = mapper.selectInfo();
+	public String getStocks(@ModelAttribute("criteria")Criteria criteria , Model model) {
+		List<Stock> stock = mapper.selectStockList(criteria);
+		System.out.println(stock);
 		model.addAttribute("stock", stock);
 		return "index";
 	}
---
+++ src/main/java/com/abcproduct/domain/paging/Criteria.java
@@ -0,0 +1,34 @@
+package com.abcproduct.domain.paging;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Criteria {
+
+	/** 現在のページ番号 */
+	private int currentPageNo;
+
+	/** ページごとに出力するデータ数 */
+	private int recordsPerPage;
+
+	/** ページサイズ*/
+	private int pageSize;
+
+	/** 検索キーワード*/
+	private String searchKeyword;
+
+	/** 検索タイプ*/
+	private String searchType;
+
+	public Criteria() {
+		this.currentPageNo = 1;
+		this.recordsPerPage = 5;
+		this.pageSize = 5;
+	}
+
+	public int getStartPage() {
+		return (currentPageNo - 1) * recordsPerPage;
+	}
+}
